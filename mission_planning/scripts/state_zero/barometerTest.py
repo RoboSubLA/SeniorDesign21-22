17ee99d52215c8ae13a69bbc313e90514738a6c5
wweng3 <holydragon77777@gmail.com>
2021-02-15 13:22:39 -0800

    added code to barometertest

--- mission_planning/scripts/state_zero/barometerTest.py
+++ mission_planning/scripts/state_zero/barometerTest.py
@@ -1,3 +1,29 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Int16, String
+import time
+import threading
+import numpy
+
+
+def callback(data):
+    barometer = data
+    rospy.loginfo(str(barometer) + ' atm')
+
+
 def barometerTest():
-    #make subscriber
-    pass
+    rospy.init_node('listener', anonymous=True)
+    sub = rospy.Subscriber("data_pub_input", Int16, callback)
+    pub = rospy.Publisher("data_recorder", String, queue_size=20)
+    pub.publish(sub)
+    rospy.spin()
+
+
+
+if __name__ == '__main__':
+    barometerTest()
+    # make pub
+    # wait for data
+    # if vaild return true
+    # else false
