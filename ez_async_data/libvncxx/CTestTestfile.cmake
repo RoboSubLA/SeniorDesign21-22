1ccb88824b824b4c44e807e638f0b314429ae932
albonkey <43227255+albonkey@users.noreply.github.com>
2021-11-15 13:38:41 -0800

    Updating Structure

--- ez_async_data/libvncxx/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /u02/vnproglib-1.1/cpp
-# Build directory: /u02/vnproglib-1.1/cpp/examples/ez_async_data/libvncxx
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
