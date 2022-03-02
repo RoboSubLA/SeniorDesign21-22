1d7a00207e1ea89f0fd126d204d90a1542f54de4
alanchan442 <70178155+alanchan442@users.noreply.github.com>
2022-03-01 16:39:35 -0800

    Update gate_init.py
    Changed what some outcomes led to.

--- mission_planning/scripts/gate_state/gate_init.py
+++ mission_planning/scripts/gate_state/gate_init.py
@@ -6,31 +6,26 @@ import smach_ros
 from utilities.lost_target import LostTarget
 from utilities.reattempt import Reattempt
 from position_sub import PositionSub
-# from mission_planning.scripts.utility_states.reset_for_reattempt import ResetForReattempt
+from mission_planning.scripts.utility_states.reset_for_reattempt import ResetForReattempt
 from execute_gate import add_ex_gate_states
 
+# gate_init.py will add all the necessary gate states to the state machine.
 def add_gate_states():
-    # ##declaration of new sub state machine###
-    # #create new sub state machine
-    # position_sub = smach.StateMachine(outcomes=['success','failed'])
-    # #add all substates to the new sub state machine using declared add_gate_states function
-    # with position_sub:
+	# Adding the first state that comes after state_zero is passed, the position_sub state, in which the AUV aligns itself with the gate, so that it may pass
+	# through the gate without any trouble.
+	
+    # position_sub = smach.StateMachine(outcomes=['success','timeout','no_target'])
+	
+	# with position_sub:
     #     add_position_sub_states()
-    # #add the new substate machine to the previous one (sm), add in the approproate transitions to new states
-    # smach.StateMachine.add('position_sub', position_sub, transitions={'success':'execute_gate', 'failed':'failed'})
-    # #note that when adding a substatemachine there is no brackets, unlike the state zero in which we call the state constructor
-    # ###end declaration of new sub state machine####
+		
+	smach.StateMachine.add('lost_target', LostTarget(), transitions={'target_found':'position_sub', 'timeout':'failed'})
+	smach.StateMachine.add('position_sub', PositionSub(), transitions={'success':'execute_gate', 'timeout':'reset_for_reattempt', 'no_target':'lost_target'})
 
-
-    smach.StateMachine.add('position_sub', PositionSub(), transitions={'success':'execute_gate', 'failed':'failed', 'timeout':'reset_for_reattempt'})
-
-
-    ##add sub state machine
-    ex_gate = smach.StateMachine(outcomes=['success', 'reset_for_reattempt'])
+	# Adding the execute gate (ex_gate) states, which will tell the AUV to begin moving towards the gate.
+    ex_gate = smach.StateMachine(outcomes=['success','failed'])
     with ex_gate:
         add_ex_gate_states()
-    smach.StateMachine.add('execute_gate', ex_gate, transitions={'success':'success', 'reset_for_reattempt':'reset_for_reattempt'})
-    ##
-
-    smach.StateMachine.add('lost_target', LostTarget(), transitions={'target_found':'position_sub', 'timeout':'failed'})
-    smach.StateMachine.add('reset_for_reattempt', Reattempt(), transitions={'complete':'position_sub', 'lost_target':'lost_target'})
+	
+	smach.StateMachine.add('execute_gate', ex_gate, transitions={'success':'success', 'failed':'reset_for_reattempt'})
+	smach.StateMachine.add('reset_for_reattempt', Reattempt(), transitions={'complete':'position_sub'})
