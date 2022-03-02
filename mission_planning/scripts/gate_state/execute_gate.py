fbf390076c453b99cc1b7d8ab44bd3fa19a30990
alanchan442 <70178155+alanchan442@users.noreply.github.com>
2022-03-01 16:37:47 -0800

    Update execute_gate.py
    This is an updated execute_gate file, but it's still incomplete, as I don't know yet what MoveForwardNoVision is supposed to do. Like all the other code, it's still untested. I'll probably test it some time this week, so there's probably still errors, but this is what I've done so far.

--- mission_planning/scripts/gate_state/execute_gate.py
+++ mission_planning/scripts/gate_state/execute_gate.py
@@ -1,31 +1,44 @@
 import smach
 import rospy
+from sensing_and_actuation.ez_async_data.msg import gate
 
 class MoveForwardVision(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['success','failed'])
-    
+		self.gate_sub = Subscriber('gate', gate)
+        # p = rospy.SubscribeListener()
+        self.rate = rospy.Rate(5)
+        self.elapsedTime = 0
+        self.maxTime = 30
+		
     def execute(self, userdata):
-        transition = 1
-        if transition == 1:
-            rospy.loginfo("success")
-            return 'success'
-        else:
-            rospy.loginfo("failed")
-            return 'failed'
+		beginningTime = time.time()
+		while(True):
+			self.elapsedTime = time.time()
+			data = self.gate_sub.get_data()
+			if data.gateseen and data.gatedis < 0.5:
+				rospy.loginfo("success")
+				return 'success'
+			elif (beginningTime - self.elapsedTime) > self.maxTime::
+				rospy.loginfo("failed")
+				return 'failed'
 
+# Incomplete.
 class MoveForwardNoVision(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['success','failed'])
-    
+		self.controls_pub = rospy.Publisher('controls', String, queue_size=10)
+	
     def execute(self, userdata):
-        transition = 1
-        if transition == 1:
-            rospy.loginfo("success")
-            return 'success'
-        else:
-            rospy.loginfo("failed")
-            return 'failed'
+        # transition = 1
+		self.controls_pub.publish('go forward')
+		return 'success'
+        # if transition == 1:
+        #     rospy.loginfo("success")
+        #     return 'success'
+        # elif (beginningTime - self.elapsedTime) < self.maxTime::
+        #     rospy.loginfo("failed")
+        #     return 'failed'
 
 def add_ex_gate_states():
     smach.StateMachine.add('move_forward_vision', MoveForwardVision(), transitions={'success':'move_forward_no_vision', 'failed':'reset_for_reattempt'})
