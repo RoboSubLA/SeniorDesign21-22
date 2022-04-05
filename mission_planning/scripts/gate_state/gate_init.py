6a3fefa65bf5d8f0d95c6fa36edbfacf3d183e1a
alanchan442 <70178155+alanchan442@users.noreply.github.com>
2022-04-05 16:38:36 -0700

    Update gate_init.py
    Somewhat working now. The nested states, "move_forward_no_vision" and "move_forward_vision" were giving me errors because they referred to a state that was outside of the container, reset_for_reattempt. To fix this, I just removed the nested states, because they didn't change anything.

--- mission_planning/scripts/gate_state/gate_init.py
+++ mission_planning/scripts/gate_state/gate_init.py
@@ -5,9 +5,12 @@ import smach_ros
 
 from utilities.lost_target import LostTarget
 from utilities.reattempt import Reattempt
+from lost_target import LostTarget
+from reset_for_reattempt import ResetForReattempt
 from position_sub import PositionSub
-from mission_planning.scripts.utility_states.reset_for_reattempt import ResetForReattempt
 from execute_gate import add_ex_gate_states
+from execute_gate import MoveForwardVision
+from execute_gate import MoveForwardNoVision
 
 # gate_init.py will add all the necessary gate states to the state machine.
 def add_gate_states():
@@ -19,13 +22,10 @@ def add_gate_states():
 	# with position_sub:
     #     add_position_sub_states()
 		
-	smach.StateMachine.add('lost_target', LostTarget(), transitions={'target_found':'position_sub', 'timeout':'failed'})
-	smach.StateMachine.add('position_sub', PositionSub(), transitions={'success':'execute_gate', 'timeout':'reset_for_reattempt', 'no_target':'lost_target'})
+    smach.StateMachine.add('reset_for_reattempt', ResetForReattempt(), transitions={'complete':'position_sub'})
+    smach.StateMachine.add('lost_target', LostTarget(), transitions={'target_found':'position_sub', 'timeout':'failed'})
+    smach.StateMachine.add('position_sub', PositionSub(), transitions={'success':'move_forward_vision', 'timeout':'reset_for_reattempt', 'no_target':'lost_target'})
 
 	# Adding the execute gate (ex_gate) states, which will tell the AUV to begin moving towards the gate.
-    ex_gate = smach.StateMachine(outcomes=['success','failed'])
-    with ex_gate:
-        add_ex_gate_states()
-	
-	smach.StateMachine.add('execute_gate', ex_gate, transitions={'success':'success', 'failed':'reset_for_reattempt'})
-	smach.StateMachine.add('reset_for_reattempt', Reattempt(), transitions={'complete':'position_sub'})
+    smach.StateMachine.add('move_forward_vision', MoveForwardVision(), transitions={'success':'move_forward_no_vision', 'failed':'reset_for_reattempt'})
+    smach.StateMachine.add('move_forward_no_vision', MoveForwardNoVision(), transitions={'success':'success', 'failed':'reset_for_reattempt'})
