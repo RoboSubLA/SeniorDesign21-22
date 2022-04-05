6605012082f82eea4cd01b785a50c1aa30a2b992
alanchan442 <70178155+alanchan442@users.noreply.github.com>
2022-04-05 16:41:29 -0700

    Update position_sub.py
    Somewhat working now. position_sub.py actually receives some data from the dummy publishers now, but some data still isn't received. This file will have to be modified again as the CV and other teams develop their code, as it's only a skeleton right now that hasn't been tested a lot.

--- mission_planning/scripts/gate_state/position_sub.py
+++ mission_planning/scripts/gate_state/position_sub.py
@@ -1,21 +1,23 @@
 import smach
 import rospy
+import time
 
 from std_msgs.msg import String
-from sensing_and_actuation.ez_async_data.msg import CV
+from ez_async_data.msg import CV
 from utilities.comms import Subscriber
-
-import time
+from dvl.msg import DVL
+from sonar.msg import Sonar
 
 #CV
 #targetSeen, targetDis, xOffset, yOffset 
 
 class PositionSub(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['success', 'failed', 'timeout'])
+        smach.State.__init__(self, outcomes=['success', 'no_target', 'timeout'])
         self.gate_sub = Subscriber('cv_data', CV)
+        self.sonar_test_sub =  Subscriber("sonar_topic", Sonar)
         self.controls_pub = rospy.Publisher('controls', String, queue_size=10)
-        # p = rospy.SubscribeListener()
+	# p = rospy.SubscribeListener()
         self.rate = rospy.Rate(5)
         self.elapsedTime = 0
         self.maxTime = 30
@@ -24,21 +26,21 @@ class PositionSub(smach.State):
 
     def execute(self, userdata):
         output = ''
-		beginningTime = time.time()
+        beginningTime = time.time()
         while(True):
             self.elapsedTime = time.time()
-
-            # Get data from CV.
+			# Get data from CV.
             data = self.gate_sub.get_data()
+            data1 = self.sonar_test_sub.get_data()
             
-			if (beginningTime - self.elapsedTime) > self.maxTime:
-				if (data.targetSeen)
-					return('timeout')
-				else
-					return('no_target')
+            if (self.elapsedTime - beginningTime) > self.maxTime:
+                if data.targetSeen:
+                    return('timeout')
+                else:
+                    return('no_target')
 				
             if data.xOffset < self.xError and data.yOffset < self.yError and data.targetSeen:
-                return 'success'
+                    return 'success'
         
             command = self.getCenterCommand(data)
             self.controls_pub.publish(command[0] + '\n'+ command[1] + '/n/n')
@@ -47,22 +49,30 @@ class PositionSub(smach.State):
             output += 'Target Dis: ' + str(data.targetDis) + '\n'
             output += 'xOffset: ' + str(data.xOffset) + '\n'
             output += 'yOffset: ' + str(data.yOffset) + '\n\n'
+            #(TEMPORARY) For debugging and testing.
+            output += 'Distance: ' + str(data1.distance) + '\n'
+            output += 'Time: ' + str(self.elapsedTime - beginningTime) + '\n'
             rospy.loginfo(output)
             self.rate.sleep()
 
     def getCenterCommand(self, data):
         setpoints = []
         if data.xOffset > self.xError:
-            # The AUV is too far right to pass through the gate. The x value has to be decreased, so a command is sent to Controls to move left, or decrease x.
-            setpoint.append('go left')
+			# The AUV is too far right to pass through the gate. The x value has to be decreased, so a command is sent to Controls to move left, or decrease x.
+            setpoints.append('go left')
         elif data.xOffset < -self.xError:
-            # The AUV is too far left to pass through the gate. The x value has to be increased, so a command is sent to Controls to move right, or increase x.
-            setpoint.append('go right')
+			# The AUV is too far left to pass through the gate. The x value has to be increased, so a command is sent to Controls to move right, or increase x.
+            setpoints.append('go right')
+        else:
+            setpoints.append('N/A')
 
         if data.yOffset > self.yError:
-            # The AUV is too high to pass through the gate. Height has to be decreased, so a command is sent to Controls to decrease height.
-            setpoint.append('go down')
+			# The AUV is too high to pass through the gate. Height has to be decreased, so a command is sent to Controls to decrease height.
+            setpoints.append('go down')
         elif data.yOffset < -self.yError:
-            # The AUV is too low to pass through the gate. Height has to be increased, so a command is sent to Controls to increase height.
-            setpoint.append('go up')
-		return setpoints
+			# The AUV is too low to pass through the gate. Height has to be increased, so a command is sent to Controls to increase height.
+            setpoints.append('go up')
+        else:
+            setpoints.append('N/A')
+        return setpoints
+
