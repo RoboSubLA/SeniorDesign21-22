f941e14d80c06e9e3743e87bda63a9582613641c
alanchan442 <70178155+alanchan442@users.noreply.github.com>
2022-04-20 15:29:49 -0700

    Update execute_gate.py

--- mission_planning/scripts/gate_state/execute_gate.py
+++ mission_planning/scripts/gate_state/execute_gate.py
@@ -2,6 +2,7 @@ import smach
 import rospy
 import time
 
+from std_msgs.msg import String
 from ez_async_data.msg import gate
 from utilities.comms import Subscriber
 
@@ -9,6 +10,7 @@ class MoveForwardVision(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['success','failed'])
         self.gate_sub = Subscriber('gate', gate)
+        self.controls_pub = rospy.Publisher('controls', String, queue_size=10)
         # p = rospy.SubscribeListener()
         self.rate = rospy.Rate(5)
         self.elapsedTime = 0
@@ -20,7 +22,11 @@ class MoveForwardVision(smach.State):
         while(True):
             self.elapsedTime = time.time()
             data = self.gate_sub.get_data()
-            if data.gateseen and data.gatedis < 0.5:
+            if data.gatediscentr > 0.5:
+                self.controls_pub.publish('align with center of gate')
+            elif (data.gatedis > 0.5):
+                self.controls_pub.publish('go forward')
+            elif (data.gateseen and data.gatedis < 0.5 and data.gatediscentr < 1):
                 rospy.loginfo("success")
                 return 'success'
             elif (self.elapsedTime - beginningTime) > self.maxTime:
@@ -31,18 +37,18 @@ class MoveForwardVision(smach.State):
 class MoveForwardNoVision(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['success','failed'])
-	# self.controls_pub = rospy.Publisher('controls', String, queue_size=10)
+        self.controls_pub = rospy.Publisher('controls', String, queue_size=10)
 	
     def execute(self, userdata):
-        # transition = 1
-	    # self.controls_pub.publish('go forward')
-        return 'success'
-        # if transition == 1:
-        #     rospy.loginfo("success")
-        #     return 'success'
-        # elif (beginningTime - self.elapsedTime) < self.maxTime::
-        #     rospy.loginfo("failed")
-        #     return 'failed'
+        beginningTime = time.time()
+
+        while(True):
+            self.elapsedTime = time.time()
+            data = self.gate_sub.get_data()
+            if (self.elapsedTime - beginningTime) < 5:
+                self.controls_pub.publish('go forward')
+            elif (self.elapsedTime - beginningTime) > 5:
+                return 'success'
 
 def add_ex_gate_states():
     smach.StateMachine.add('move_forward_vision', MoveForwardVision(), transitions={'success':'move_forward_no_vision', 'failed':'reset_for_reattempt'})
