4c808040ef35a909fc6afd3e2d5206f75181322a
albonkey <carl_solli@hotmail.com>
2022-05-29 21:36:07 -0700

    Updated to gate_task readme

--- mission_planning/state_machine/gate_task/gate_task.py
+++ mission_planning/state_machine/gate_task/gate_task.py
@@ -34,24 +34,15 @@ class MoveForwardVision(smach.State):
         smach.State.__init__(self, outcomes=['success','failed'])
 
     def execute(self, userdata):
-        transition = 1
-        if transition == 1:
-            rospy.loginfo("success")
-            return 'success'
-        else:
-            rospy.loginfo("failed")
-            return 'failed'
+        #Move forward while cv object x and y is center of frame.
+        return 'success'
+        
 
 class MoveForwardNoVision(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['success','failed'])
 
     def execute(self, userdata):
-        transition = 1
-        if transition == 1:
-            rospy.loginfo("success")
-            return 'success'
-        else:
-            rospy.loginfo("failed")
-            return 'failed'
+       #Move forward
+       return 'success'
 
--- mission_planning/state_machine/gate_task/readme.md
+++ mission_planning/state_machine/gate_task/readme.md
@@ -1,1 +1,4 @@
 # Gate Task
+This is the state machine for the gate task. You can see how we have structured it under.
+
+![Gate Task Design](https://github.com/RoboSubCSULA/SeniorDesign21-22/tree/main/mission_planning/state_machine/gate_task/gate_task_design.png)
